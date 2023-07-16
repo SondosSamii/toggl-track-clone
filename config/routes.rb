30c42bb08d482ee011dea96b858c3c3605c4c1dd
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2023-07-16 16:11:53 +0300

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
 
   # Defines the root path route ("/")
   # root "articles#index"
