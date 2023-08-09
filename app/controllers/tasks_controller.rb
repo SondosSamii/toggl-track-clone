2b1a4febe619e8c2d08cb154f69c7a6ee09d569d
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2023-08-09 10:50:43 +0300

    Remove: 'due_date' from task_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -72,6 +72,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :description, :due_date, :project_id)
+      params.require(:task).permit(:title, :description, :project_id, :total_time_elapsed)
     end
 end
